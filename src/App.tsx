3a0941778e20fa9bbe569f892f2441a02f16c256
OnyGooD <molnekkrisz@gmail.com>
2026-02-24 09:14:36 +0100

    add base desgin

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ function App() {
 
   return (
     <>
-    <h1>Hello</h1>
+    <div className="container">
+      <img src="public\images\icon-work.svg" id="icon"/>
+      <div className="displayBox">
+        <p id="title">Work</p>
+        <img id="dotButton" src="public\images\icon-ellipsis.svg"/>
+        <p id="hrsTime">32hrs</p>
+        <p id="lastWeek">Last Week - 36hrs</p>
+      </div>
+    </div>
     </>
   )
 }
